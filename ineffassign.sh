ce3b964014b15307a641d90f1679f18050db47fa
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-03 08:34:12 +0200

    Fix ineffassign on CI

--- ineffassign.sh
+++ ineffassign.sh
@@ -24,7 +24,7 @@ echo -e "${BLUE}Detecting ineffectual assignments in Go code${NC}"
 if ! [ -x "$(command -v ineffassign)" ]
 then
     echo -e "${BLUE}Installing ineffassign${NC}"
-    GO111MODULE=off go get github.com/gordonklaus/ineffassign
+    go install github.com/gordonklaus/ineffassign
 fi
 
 if ! ineffassign ./...
