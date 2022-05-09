8cc6f71e65e621e56a9b77c698f0680a87047346
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-09 08:08:35 +0200

    Unit tests for function ReadTable

--- storage_test.go
+++ storage_test.go
@@ -17,8 +17,13 @@ limitations under the License.
 package main_test
 
 import (
+	"errors"
+	"io/ioutil"
 	"testing"
 
+	"database/sql"
+
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/stretchr/testify/assert"
 
 	main "github.com/RedHatInsights/insights-results-aggregator-exporter"
@@ -75,4 +80,140 @@ func TestClose(t *testing.T) {
 
 	// it should not fail
 	assert.Nil(t, err)
+}
+
+// mustCreateMockConnection function tries to create a new mock connection and
+// checks if the operation was finished without problems.
+func mustCreateMockConnection(t *testing.T) (*sql.DB, sqlmock.Sqlmock) {
+	// try to initialize new mock connection
+	connection, mock, err := sqlmock.New()
+
+	// check the status
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	return connection, mock
+}
+
+// checkConnectionClose function perform mocked DB closing operation and checks
+// if the connection is properly closed from unit tests.
+func checkConnectionClose(t *testing.T, connection *sql.DB) {
+	// connection to mocked DB needs to be closed properly
+	err := connection.Close()
+
+	// check the error status
+	if err != nil {
+		t.Fatalf("error during closing connection: %v", err)
+	}
+}
+
+// checkAllExpectations function checks if all database-related operations have
+// been really met.
+func checkAllExpectations(t *testing.T, mock sqlmock.Sqlmock) {
+	// check if all expectations were met
+	err := mock.ExpectationsWereMet()
+
+	// check the error status
+	if err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+}
+
+// Expected queries
+const (
+	readRecordCountQuery = "SELECT count\\(\\*\\) FROM TESTED_TABLE"
+)
+
+// check the function ReadRecordCount
+func TestReadRecordCount(t *testing.T) {
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	// prepare mocked result for SQL query
+	rowsCount := sqlmock.NewRows([]string{"count"})
+	expected := 100
+	rowsCount.AddRow(expected)
+
+	// expected query performed by tested function
+	mock.ExpectQuery(readRecordCountQuery).WillReturnRows(rowsCount)
+	mock.ExpectClose()
+
+	// prepare connection to mocked database
+	storage := main.NewFromConnection(connection, 1)
+
+	// call the tested method
+	count, err := storage.ReadRecordsCount("TESTED_TABLE")
+	if err != nil {
+		t.Errorf("error was not expected %s", err)
+	}
+
+	if count != expected {
+		t.Errorf("wrong number records returned: %d", count)
+	}
+
+	// connection to mocked DB needs to be closed properly
+	checkConnectionClose(t, connection)
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
+
+func TestReadRecordCountScanError(t *testing.T) {
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	// prepare mocked result for SQL query
+	rowsCount := sqlmock.NewRows([]string{"count"})
+	rowsCount.AddRow("this is not integer")
+
+	// expected query performed by tested function
+	mock.ExpectQuery(readRecordCountQuery).WillReturnRows(rowsCount)
+	mock.ExpectClose()
+
+	// prepare connection to mocked database
+	storage := main.NewFromConnection(connection, 1)
+
+	// call the tested method
+	_, err := storage.ReadRecordsCount("TESTED_TABLE")
+	if err == nil {
+		t.Errorf("error is expected")
+	}
+
+	// connection to mocked DB needs to be closed properly
+	checkConnectionClose(t, connection)
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
+}
+
+func TestReadRecordCountOnError(t *testing.T) {
+	// error to be thrown
+	mockedError := errors.New("mocked error")
+
+	// prepare new mocked connection to database
+	connection, mock := mustCreateMockConnection(t)
+
+	// expected query performed by tested function
+	mock.ExpectQuery(readRecordCountQuery).WillReturnError(mockedError)
+	mock.ExpectClose()
+
+	// prepare connection to mocked database
+	storage := main.NewFromConnection(connection, 1)
+
+	// call the tested method
+	count, err := storage.ReadRecordsCount("TESTED_TABLE")
+	if err != mockedError {
+		t.Errorf("different error was returned: %v", err)
+	}
+
+	if count != -1 {
+		t.Errorf("wrong number records returned: %d", count)
+	}
+
+	// connection to mocked DB needs to be closed properly
+	checkConnectionClose(t, connection)
+
+	// check if all expectations were met
+	checkAllExpectations(t, mock)
 }
