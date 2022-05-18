1dcb4f84490b2f3739a248c9cab7779a1c83b31e
Juan Díaz <juandiazsuarez0@gmail.com>
2022-05-18 08:45:06 +0200

    [CCXDEV-8122] Add build_deploy.sh (#103)
    * add build_deploy.sh
    
    * add pr-check too

---
+++ pr_check.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# --------------------------------------------
+# Options that must be configured by app owner
+# --------------------------------------------
+APP_NAME="ccx-data-pipeline"  # name of app-sre "application" folder this component lives in
+COMPONENT_NAME="insights-results-aggregator-exporter"  # name of app-sre "resourceTemplate" in deploy.yaml for this component
+IMAGE="quay.io/cloudservices/insights-results-aggregator-exporter"
+COMPONENTS="insights-results-aggregator-exporter ccx-notification-service ccx-notification-db-cleaner"  # space-separated list of components to laod
+COMPONENTS_W_RESOURCES="insights-results-aggregator-exporter"  # component to keep
+CACHE_FROM_LATEST_IMAGE="true"
+
+export IQE_PLUGINS="ccx"
+export IQE_MARKER_EXPRESSION=""
+# Workaround: There are no cleaner specific integration tests. Check that the service loads and iqe plugin works.
+export IQE_FILTER_EXPRESSION="test_plugin_accessible"
+export IQE_REQUIREMENTS_PRIORITY=""
+export IQE_TEST_IMPORTANCE=""
+export IQE_CJI_TIMEOUT="30m"
+
+
+function build_image() {
+    source $CICD_ROOT/build.sh
+}
+
+function deploy_ephemeral() {
+    source $CICD_ROOT/deploy_ephemeral_env.sh
+}
+
+function run_smoke_tests() {
+    source $CICD_ROOT/cji_smoke_test.sh
+}
+
+
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+echo "creating PR image"
+build_image
+
+echo "deploying to ephemeral"
+deploy_ephemeral
+
+echo "running PR smoke tests"
+run_smoke_tests
